5d761c6a9e9f386cdb20be0ab6c901ffc288a237
Salem Hilal <salemhilal@gmail.com>
2020-12-04 22:00:34 -0500

    Initial commit, day 1 part 1

---
+++ days/day1/src/main.rs
@@ -0,0 +1,24 @@
+use itertools::Itertools;
+use std::fs;
+
+fn main() {
+    let input = fs::read_to_string("src/input.txt").expect("Something went wrong reading the file");
+
+    let result = input
+        .lines()
+        .map(|input| input.parse::<u32>())
+        .filter_map(Result::ok)
+        .combinations(2)
+        .filter(|combination| combination.iter().sum::<u32>() == 2020)
+        .next();
+
+    match result {
+        None => println!("Found nothing!"),
+        Some(numbers) => println!(
+            "Found {:?}, sum: {}, product: {}",
+            numbers,
+            numbers.iter().sum::<u32>(),
+            numbers.iter().product::<u32>()
+        ),
+    }
+}
